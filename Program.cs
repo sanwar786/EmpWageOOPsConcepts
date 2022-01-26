04a198fa3646bd55b2b339ac20d464a9c05da29e
sanwar786 <95489644+sanwar786@users.noreply.github.com>
2022-01-26 18:15:23 +0530

    Comuting Emp Wage

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageOOPsConcepts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Check emp is Present or Absent
+            //EmpPresentUC1.FindEmpPresent();
+            //CalculatingEmpWageUC2.FindEmpWage();
+            //PartTimeEmpUC3.FindPartTimeEmpWage();
+            //EmpWageSwitch.FindEmpWageSwitch();
+            //WageMonthUC5.FindEmpWageForMonth();
+            WageNumDayHourUC6.FindWageofWorkingDays();
+
+            Console.ReadLine();
+        }
+    }
+}
